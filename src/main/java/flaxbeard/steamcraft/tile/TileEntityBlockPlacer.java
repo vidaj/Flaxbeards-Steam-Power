d0a4b4bda9b44f4745691e25b0981f59df968ea0
Zenith <the.number.zero@gmail.com>
2014-08-08 00:35:24 -0400

    Placeholder commit

--- src/main/java/flaxbeard/steamcraft/tile/TileEntityBlockPlacer.java
+++ src/main/java/flaxbeard/steamcraft/tile/TileEntityBlockPlacer.java
@@ -1,5 +1,6 @@
 package flaxbeard.steamcraft.tile;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
@@ -7,11 +8,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import flaxbeard.steamcraft.api.tile.SteamTransporterTileEntity;
+import flaxbeard.steamcraft.api.util.Coord4;
 
 public class TileEntityBlockPlacer extends SteamTransporterTileEntity implements IInventory{
 	
 	private ItemStack[] inventory = new ItemStack[1];
 	int tick = 0;
+	int meta = -1;
+	Coord4 target;
+	
+	Block placingBlock;
+	int placingMeta;
 	
 	@Override
 	public int getSizeInventory() {
@@ -102,7 +109,36 @@ public class TileEntityBlockPlacer extends SteamTransporterTileEntity implements
 	@Override
 	public void updateEntity(){
 		super.updateEntity();
-		if (tick == 0 && )
+		
+		if (this.meta != this.getBlockMetadata()){
+			this.meta = this.getBlockMetadata();
+			this.target = this.getTarget();
+		}
+		
+		if (this.target == null){
+			this.target = this.getTarget();
+		}
+		
+		
+		if (this.hasItem() && worldObj.isAirBlock(target.x, target.y, target.z) && worldObj.getTileEntity(target.x, target.y, target.z) == null){
+			if (tick == 0){
+				this.tick++;
+				this.markForUpdate();
+			} else if (tick < 20){
+				this.tick++;
+			} else {
+				this.tick = 0;
+				
+			}
+		} else {
+			this.tick = 0;
+			this.markForUpdate();
+		}
+		
+	}
+	
+	private Coord4 getTarget(){
+		return null;
 	}
 	
 	
