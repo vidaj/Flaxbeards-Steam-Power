be85779538819826b3a4549e04723560b20f1868
Striking <Strikingwolf2012@gmail.com>
2015-11-14 20:26:43 -0600

    Refactor CanisterHelper

--- src/main/java/flaxbeard/steamcraft/handler/CanisterHandler.java
+++ src/main/java/flaxbeard/steamcraft/handler/CanisterHandler.java
@@ -1,12 +1,18 @@
 package flaxbeard.steamcraft.handler;
 
 import flaxbeard.steamcraft.SteamcraftItems;
+import flaxbeard.steamcraft.misc.Tuple;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.oredict.RecipeSorter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CanisterHandler implements IRecipe {
 
@@ -14,63 +20,47 @@ public class CanisterHandler implements IRecipe {
         RecipeSorter.register("Steamcraft:canisterHandler", CanisterHandler.class, RecipeSorter.Category.SHAPELESS, "after:minecraft:shapeless");
     }
 
-    @Override
-    public boolean matches(InventoryCrafting inv, World world) {
+    private Tuple<Boolean, ItemStack> craftHelper(InventoryCrafting inv) {
         ItemStack output = null;
         boolean hasCan = false;
         boolean canCraft = true;
-        for (int i = 0; i < inv.getSizeInventory(); i++) {
-            if (inv.getStackInSlot(i) != null) {
-                ItemStack stack = inv.getStackInSlot(i);
-                if (stack.getItem() == SteamcraftItems.canister) {
-                    if (hasCan == false) {
-                        hasCan = true;
-                    } else {
-                        canCraft = false;
-                    }
+
+        List<ItemStack> list = IntStream.range(0, inv.getSizeInventory())
+            .parallel()
+            .mapToObj(inv::getStackInSlot)
+            .filter(stack -> stack != null)
+            .collect(Collectors.toList());
+        for (ItemStack stack : list) {
+            if (stack.getItem() == SteamcraftItems.canister) {
+                if (!hasCan) {
+                    hasCan = true;
                 } else {
-                    if (output == null) {
-                        output = stack.copy();
-                    } else {
-                        canCraft = false;
-                    }
+                    canCraft = false;
+                }
+            } else {
+                if (output == null) {
+                    output = stack.copy();
+                } else {
+                    canCraft = false;
                 }
             }
         }
 
-        if (canCraft && hasCan && output != null) {
-            if (output.hasTagCompound() && output.stackTagCompound.hasKey("canned")) {
-                return false;
-            }
-            return true;
-        }
-        return false;
+        return new Tuple<>(canCraft && hasCan && output != null, output);
+    }
+
+    @Override
+    public boolean matches(InventoryCrafting inv, World world) {
+        Tuple<Boolean, ItemStack> result = craftHelper(inv);
+        ItemStack output = result.second();
+        return result.first() && !(output.hasTagCompound() && output.stackTagCompound.hasKey("canned"));
     }
 
     @Override
     public ItemStack getCraftingResult(InventoryCrafting inv) {
-        ItemStack output = null;
-        boolean hasCan = false;
-        boolean canCraft = true;
-        for (int i = 0; i < inv.getSizeInventory(); i++) {
-            if (inv.getStackInSlot(i) != null) {
-                ItemStack stack = inv.getStackInSlot(i);
-                if (stack.getItem() == SteamcraftItems.canister) {
-                    if (hasCan == false) {
-                        hasCan = true;
-                    } else {
-                        canCraft = false;
-                    }
-                } else {
-                    if (output == null) {
-                        output = stack.copy();
-                    } else {
-                        canCraft = false;
-                    }
-                }
-            }
-        }
-        if (canCraft && hasCan && output != null) {
+        Tuple<Boolean, ItemStack> result = craftHelper(inv);
+        ItemStack output = result.second();
+        if (result.first()) {
             if (output.hasTagCompound() && output.stackTagCompound.hasKey("canned")) {
                 return null;
             }
---
+++ src/main/java/flaxbeard/steamcraft/misc/Tuple.java
@@ -0,0 +1,19 @@
+package flaxbeard.steamcraft.misc;
+
+public class Tuple<F, S> {
+	private F first;
+	private S second;
+
+	public Tuple(F first, S second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public F first() {
+		return first;
+	}
+
+	public S second() {
+		return second;
+	}
+}
